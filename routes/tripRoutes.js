5af71b29a42fdf4b1eadcbd552a2c913091047d0
Manish6906 <manish980573@gmail.com>
2025-12-27 16:12:30 +0530

    vbjc

--- routes/tripRoutes.js
+++ routes/tripRoutes.js
@@ -3,6 +3,6 @@ import { submitTripForm } from "../controllers/tripController.js";
 
 const router = express.Router();
 
-router.post("/trip", submitTripForm);
+router.post("/trip-register", submitTripForm);
 
 export default router;
--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+import "dotenv/config";
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
@@ -7,19 +8,17 @@ dotenv.config();
 
 const app = express();
 
-// Middlewares
 app.use(cors());
 app.use(express.json());
 
-// Routes
-app.get("/get", (( req, res)=>
+
+app.use("/get", ((req, res)=>
 {
   console.log("hello");
   
 }))
 app.use("/api", tripRoutes);
 
-// Server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
